a3d7a46c2b97740f29c794967db279e47dbd8b58
JulesRuby <JulesRuby@users.noreply.github.com>
2020-03-06 18:38:31 -0700

    finished and cleaned up website, commit for heroku deploy

--- app.js
+++ app.js
@@ -3,52 +3,54 @@ const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
 const moment = require('moment');
+
 // Establish my personal modules
 const pageAttributes = require('./pageAttributes')
 
+// Establish variables 
+// assign a striong to dateYear to pass as an argument into the format() of the moment()
 const dateYear = "YYYY";
 
 const app = express();
 
+// make these accessible throughout my ejs 
 app.locals.moment = require('moment');
 app.locals.dateYear = dateYear;
-// console.log(moment().format(dateYear));
 
 // Define the 'view engine' that we'll be using. In this case it will be ejs, but it could be something like pug or handlebars
 app.set('view engine', 'ejs');
 
-// Retrieve information from pageAttributes to render index.ejs
+// Retrieve information from pageAttributes to render and use template varibles throughout the website
 app.get('/', function(req, res) {
   res.render('index', pageAttributes.index);
 });
 
-// Retrieve information from pageAttributes to render about.ejs
 app.get('/about', function(req, res) {
   res.render('about', pageAttributes.about);
 });
 
-// Retrieve information from pageAttributes to render gallery.ejs
 app.get('/gallery', function(req, res) {
   res.render('gallery', pageAttributes.gallery);
 });
 
-// Retrieve information from pageAttributes to render blog.ejs
 app.get('/blog', function(req, res) {
   res.render('blog', pageAttributes.blog);
 });
 
 
-app.use(express.static(path.join(__dirname, 'public'))); // Research a little more into how this mechanism operates, I only kind of get it
-// Also it's worth mentioning that the path.join thing will result in an implicit next() if the specified directory is not found
+app.use(express.static(path.join(__dirname, 'public'))); 
+// read more on how this mechanism operates, I only kind of get it
+// NOTE TO SELF: the path.join will result in an implicit next() if the specified directory is not found
 
 app.use(function(req,res,next) {
   res.status(404);
   res.send(`404 not found, maybe I specified the wrong directory, or it doesn't exist... yet?`);
 });
 
-const PORT = process.env.PORT || 3000; // I have to look into this snippet as well, it was glossed over pretty quick
-// RIGHT, I think that the above code indicated that the PORT is equal to the PORT that the environment/process is working on. If not specified, the port defaults to being 3000
+
+// PORT defaults to 3000 unless a different one is specified
+const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, function() {
-  console.log(`console log indicating that this is listening to the specified PORT, which is ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
--- pageAttributes.js
+++ pageAttributes.js
@@ -1,26 +1,26 @@
 const pageAttributes = {
   index: {
-    title: 'Home Page', // perhaps I could go deeper with this and use literals fed from client inputs? Or nah...
+    title: 'Home Page',
     intro: 'This is a big slimy index--EWWWWW',
-    content: 'YARP YARP YARP'
+    content: 'Whoooooa a landing page'
   },
 
   about: {
     title: 'About',
-    intro: 'YA YA YA get diz munny',
+    intro: 'My super special intro!',
     content: 'Time to tell you about all the mistakes I\'ve made.'
   },
 
   gallery: {
     title: 'Gallery',
-    intro: 'Looking like a snack',
+    intro: 'The bestest gallery',
     content: 'My hawt selfies'
   },
 
   blog: {
     title: 'Blog',
     intro: 'Living my best life',
-    content: 'I have done so many cools things, here are some.'
+    content: 'I have done so many cool things, you don\'t even know, but you will.'
   }
 }
 
